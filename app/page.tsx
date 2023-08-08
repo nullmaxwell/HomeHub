6d77dce9be14bbc277b382c6d60486c206310d9e
nullmaxwell <nullmaxwell@protonmail.com>
2023-08-07 22:19:36 -0500

    Obfuscated page title to environment variable.

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,19 @@ import WidgetBar from "./components/WidgetsBar";
 import jsonDataWidgets from "./data/widgetData";
 import jsonData from "./data/linkData";
 
-
 export default function Home() {
   return (
     <main className="relative h-screen">
       <div id="grain-slider" className="demo-wrap">
         <div className="md:mb-20 mb-10 w-full flex flex-col justify-center items-center drop-shadow-lg relative z-20">
-          <h1 className="md:text-4xl text-2xl font-black text-center text-white font-mono md:my-20 my-10">Amyna Data Infra
-            Resources</h1>
+          <h1 className="md:text-4xl text-2xl font-black text-center text-white font-mono md:my-20 my-10">
+            {process.env.HUB_PAGE_TITLE}
+          </h1>
           <WidgetBar data={jsonDataWidgets} />
         </div>
         <LinkContainer data={jsonData} />
         <Settings />
       </div>
     </main>
-  )
+  );
 }
